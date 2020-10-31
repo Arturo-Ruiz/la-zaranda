b4439d99bd6c8d3dcb66af83ed5a5e35699c4c59
Arturo <gregory09ruiz@gmail.com>
2020-10-31 19:37:27 -0400

    test

---
+++ controladores/productos.controlador.php
@@ -0,0 +1,288 @@
+<?php
+
+class ControladorProductos{
+
+	/*=============================================
+	MOSTRAR PRODUCTOS
+	=============================================*/
+
+	static public function ctrMostrarProductos($item, $valor, $orden){
+
+		$tabla = "productos";
+
+		$respuesta = ModeloProductos::mdlMostrarProductos($tabla, $item, $valor, $orden);
+
+		return $respuesta;
+
+	}
+
+	/*=============================================
+	CREAR PRODUCTO 
+	=============================================*/
+
+	static public function ctrCrearProducto(){
+
+		if(isset($_POST["nuevaDescripcion"])){
+
+			if(preg_match('/^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ ]+$/', $_POST["nuevaDescripcion"]) &&
+			   preg_match('/^[0-9]+$/', $_POST["nuevoStock"]) &&	
+			   preg_match('/^[0-9.]+$/', $_POST["nuevoPrecioCompra"]) &&
+			   preg_match('/^[0-9.]+$/', $_POST["nuevoPrecioVenta"])){
+
+				$tabla = "productos";
+
+				$datos = array("id_categoria" => $_POST["nuevaCategoria"],
+							   "codigo" => $_POST["nuevoCodigo"],
+							   "descripcion" => $_POST["nuevaDescripcion"],
+							   "stock" => $_POST["nuevoStock"],
+							   "precio_compra" => $_POST["nuevoPrecioCompra"],
+							   "precio_venta" => $_POST["nuevoPrecioVenta"]);
+
+				$respuesta = ModeloProductos::mdlIngresarProducto($tabla, $datos);
+
+				if($respuesta == "ok"){
+
+					echo'<script>
+
+						swal({
+							  type: "success",
+							  title: "El producto ha sido guardado correctamente",
+							  showConfirmButton: true,
+							  confirmButtonText: "Cerrar"
+							  }).then(function(result){
+										if (result.value) {
+
+										window.location = "productos";
+
+										}
+									})
+
+						</script>';
+
+				}
+
+
+			}else{
+
+				echo'<script>
+
+					swal({
+						  type: "error",
+						  title: "¡El producto no puede ir con los campos vacíos o llevar caracteres especiales!",
+						  showConfirmButton: true,
+						  confirmButtonText: "Cerrar"
+						  }).then(function(result){
+							if (result.value) {
+
+							window.location = "productos";
+
+							}
+						})
+
+			  	</script>';
+			}
+		}
+
+	}
+
+	/*=============================================
+	EDITAR PRODUCTO
+	=============================================*/
+
+	static public function ctrEditarProducto(){
+
+		if(isset($_POST["editarDescripcion"])){
+
+			if(preg_match('/^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ ]+$/', $_POST["editarDescripcion"]) &&
+			   preg_match('/^[0-9]+$/', $_POST["editarStock"]) &&	
+			   preg_match('/^[0-9.]+$/', $_POST["editarPrecioCompra"]) &&
+			   preg_match('/^[0-9.]+$/', $_POST["editarPrecioVenta"])){
+
+
+				$tabla = "productos";
+
+				$datos = array("id_categoria" => $_POST["editarCategoria"],
+							   "codigo" => $_POST["editarCodigo"],
+							   "descripcion" => $_POST["editarDescripcion"],
+							   "stock" => $_POST["editarStock"],
+							   "precio_compra" => $_POST["editarPrecioCompra"],
+							   "precio_venta" => $_POST["editarPrecioVenta"]);
+
+				$respuesta = ModeloProductos::mdlEditarProducto($tabla, $datos);
+
+				if($respuesta == "ok"){
+
+					echo'<script>
+
+						swal({
+							  type: "success",
+							  title: "El producto ha sido editado correctamente",
+							  showConfirmButton: true,
+							  confirmButtonText: "Cerrar"
+							  }).then(function(result){
+										if (result.value) {
+
+										window.location = "productos";
+
+										}
+									})
+
+						</script>';
+
+				}
+
+
+			}else{
+
+				echo'<script>
+
+					swal({
+						  type: "error",
+						  title: "¡El producto no puede ir con los campos vacíos o llevar caracteres especiales!",
+						  showConfirmButton: true,
+						  confirmButtonText: "Cerrar"
+						  }).then(function(result){
+							if (result.value) {
+
+							window.location = "productos";
+
+							}
+						})
+
+			  	</script>';
+			}
+		}
+
+	}
+
+	/*=============================================
+	BORRAR PRODUCTO
+	=============================================*/
+	static public function ctrEliminarProducto(){
+
+		if(isset($_GET["idProducto"])){
+
+			$tabla ="productos";
+			$datos = $_GET["idProducto"];
+
+
+			$respuesta = ModeloProductos::mdlEliminarProducto($tabla, $datos);
+
+			if($respuesta == "ok"){
+
+				echo'<script>
+
+				swal({
+					  type: "success",
+					  title: "El producto ha sido borrado correctamente",
+					  showConfirmButton: true,
+					  confirmButtonText: "Cerrar"
+					  }).then(function(result){
+								if (result.value) {
+
+								window.location = "productos";
+
+								}
+							})
+
+				</script>';
+
+			}		
+		}
+
+
+	}
+
+	/*=============================================
+	MOSTRAR SUMA VENTAS
+	=============================================*/
+
+	static public function ctrMostrarSumaVentas(){
+
+		$tabla = "productos";
+
+		$respuesta = ModeloProductos::mdlMostrarSumaVentas($tabla);
+
+		return $respuesta;
+
+	}
+
+
+
+		/*=============================================
+	DESCARGAR EXCEL
+	=============================================*/
+
+	public function ctrDescargarReporteProductos(){
+
+
+				$item = null;
+				$valor = null;
+				$tabla = "productos";
+				$orden = 'codigo';
+
+				$productos = ModeloProductos::mdlMostrarProductos($tabla, $item, $valor, $orden);
+
+			
+				// echo json_encode($productos);   
+
+			/*=============================================
+			CREAMOS EL ARCHIVO DE EXCEL
+			=============================================*/
+			$reporte = 'Reporte de Productos';
+			$Name = $reporte . '.xls';
+
+			header('Expires: 0');
+			header('Cache-control: private');
+			header("Content-type: application/vnd.ms-excel"); // Archivo de Excel
+			header("Cache-Control: cache, must-revalidate"); 
+			header('Content-Description: File Transfer');
+			header('Last-Modified: '.date('D, d M Y H:i:s'));
+			header("Pragma: public"); 
+			header('Content-Disposition:; filename="'.$Name.'"');
+			header("Content-Transfer-Encoding: binary");
+		
+			echo utf8_decode("<table border='0'> 
+
+					<tr> 
+					<td style='font-weight:bold; border:1px solid #eee;'>Codigo</td> 
+					<td style='font-weight:bold; border:1px solid #eee;'>Categoria</td>
+					<td style='font-weight:bold; border:1px solid #eee;'>Descripcion</td>
+					<td style='font-weight:bold; border:1px solid #eee;'>Stock</td>
+					<td style='font-weight:bold; border:1px solid #eee;'>Precio de compra</td>
+					<td style='font-weight:bold; border:1px solid #eee;'>Precio de venta</td>
+					<td style='font-weight:bold; border:1px solid #eee;'>Ventas</td>		
+					</tr>");
+
+			foreach ($productos as $row => $item){
+
+	
+				$categoria = ControladorCategorias::ctrMostrarCategorias_id("id", $item["id_categoria"]);
+
+
+			 echo utf8_decode("<tr>
+			 			<td style='border:1px solid #eee;'>".$item["codigo"]."</td> 
+			 			<td style='border:1px solid #eee;'>".$categoria["categoria"]."</td> 
+						<td style='border:1px solid #eee;'>".$item["descripcion"]."</td> 
+						<td style='border:1px solid #eee;'>".$item["stock"]."</td> 
+						<td style='border:1px solid #eee;'>$ ".number_format($item["precio_compra"],2)."</td>
+						<td style='border:1px solid #eee;'>$ ".number_format($item["precio_venta"],2)."</td>
+						<td style='border:1px solid #eee;'>".$item["ventas"]."</td> 					 
+						 ");
+
+			
+		 		echo utf8_decode("
+		 			</tr>");
+
+
+			}
+
+
+			echo "</table>";
+
+		}
+
+	}
+
+
+
---
+++ vistas/modulos/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+	
+	<strong>Copyright &copy; 2020 <a href="https://artic-solutions.github.io/web/" target="_blank">Artic Solutions</a>.</strong>
+
+	Todos los derechos reservados.
+
+
+</footer>
